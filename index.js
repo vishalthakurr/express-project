60db45d4bbc2fde21d2eccdde7581e6bf22c504f
vishal thakur <vishalthakurr302@gmail.com>
2021-06-27 13:36:17 +0530

    upate

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-const port =5000;
+const port = process.env.PORT || 8000;
 
 // console.log(path.join(__dirname,"./public"))
 const staticpath = path.join(__dirname,"./public");
